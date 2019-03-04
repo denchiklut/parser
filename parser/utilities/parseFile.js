e92659280a3a806e8a48c1c85de775057670b481
denis <denchiklut@gmail.com>
2019-03-04 12:13:41 +0300

    #Fixed sync requests

--- parser/utilities/parseFile.js
+++ parser/utilities/parseFile.js
@@ -1,16 +1,16 @@
-let request = require('request');
 let cheerio = require("cheerio");
 const xml2js = require('xml2js')
 const fs = require('fs')
 const https = require('https');
 const PageData = require('../../config').pageData
 
+// const proxy = require('./proxyArr').proxy
+
 function downloadPage(url) {
     return new Promise((resolve, reject) => {
-
         let proxyUrl = '';
         let options = {};
-        https.get('https://api.getproxylist.com/proxy', (resp) => {
+        https.get('https://api.getproxylist.com/proxy?apiKey=0b0cd4c3f63c807e148c97c6d70dc0d54b998370', (resp) => {
             let data = '';
 
             // A chunk of data has been recieved.
@@ -21,40 +21,49 @@ function downloadPage(url) {
             // The whole response has been received. Print out the result.
             resp.on('end', () => {
                 proxyUrl = `${JSON.parse(data).ip}: ${JSON.parse(data).port}`
+
+                //добовлние прокси в конец массива
+
+
+                fs.appendFileSync("proxy.txt", `\n${proxyUrl}`  )
                 console.log(proxyUrl)
+
                 options = {
                     headers: {
                         'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
                     },
-                    proxy: proxyUrl
+                    proxy: proxyUrl,
                 };
-
+                let request = require('request')
+                request = request.defaults({jar: true})
                 request(options, function () {
                     request(url, (error, response, body) => {
-                        if (error) reject(error);
-                        if (response.statusCode != 200) {
-                            reject('Invalid status code <' + response.statusCode + '>');
+                        if (error) console.log(error)
+
+                        try {
+
+                            const $ = cheerio.load(body);
+                            const webpageTitle = $("title").text();
+                            const metaDescription =  $('meta[name=description]').attr("content");
+                            const keywords =  $('meta[name=keywords]').attr("content");
+                            const result = {
+                                site: url,
+                                title: webpageTitle,
+                                description: metaDescription,
+                                keywords: keywords,
+                            }
+
+                            let log = `--- SUCCESS ---: ${result.site}`
+                            console.log(log)
+
+
+                            // //Пишем в БД результаты работы парсера
+                            let msg = new PageData({pageData: result})
+                           msg.save()
+                            resolve(body);
+                        } catch (error) {
+                            reject(error)
                         }
-
-                        const $ = cheerio.load(body);
-                        const webpageTitle = $("title").text();
-                        const metaDescription =  $('meta[name=description]').attr("content");
-                        const keywords =  $('meta[name=keywords]').attr("content");
-                        const result = {
-                            site: url,
-                            title: webpageTitle,
-                            description: metaDescription,
-                            keywords: keywords,
-                        }
-
-                        let log = `--- SUCCESS ---: ${result.site}`
-                        console.log(log)
-
-
-                        // //Пишем в БД результаты работы парсера
-                        // let msg = new PageData({pageData: result})
-                        // msg.save()
-                        resolve(body);
                     });
                 });
             })
@@ -75,26 +84,23 @@ exports.parseFile = (rootDir, item) => {
     parser.parseString(content, (err, result) => {
         ( async ()=>{
 
-            //Для теста обрезаем массив
-            let arr = result.urlset.url.slice(21000, 21005)
-            // let arr = result.urlset.url
+            let length = result.urlset.url.length
+
+            let arr = result.urlset.url.slice(686 + 251 + 233, length)
 
             let counter = 0;
             let size = arr.length
 
-            for (var item = 0; item < arr.length; item++) {
+            for (var item = 0; item < size; item++) {
 
                 console.log(counter)
                 try {
-
-                    console.log(arr[item].loc[0])
-                    await downloadPage(arr[item].loc[0]).then(()=> counter++)
-
-
+                    await downloadPage(arr[item].loc[0])
+                        .then(() => counter++)
                 } catch (err) {
-                    console.log(err)
 
-                    let error = `---  ERROR ---: ${arr[item].loc[0]}`
+                    console.log(err)
+                    let error = `---  ERROR  ---: ${arr[item].loc[0]}`
                     console.log(error)
                 }
 
